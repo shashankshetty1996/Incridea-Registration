e83f18dafd3a8a62b3e3a6218c136a7c65507e50
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-12-12 22:41:15 +0530

    Basic login UI for login page with angular basic setup

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,3 +1,13 @@
 angular.module('myApp').controller('loginController',function($scope,$http) {
     $scope.msg='Login Page';
+
+    $scope.login = function() {
+        let params = {
+            "username" : $scope.username,
+            "password" : $scope.password
+        };
+
+        $scope.username = "";
+        $scope.password = "";
+    }
 });
