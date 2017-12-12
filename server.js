e30425ccded89df7ddb9f6e98db3efb26ecdc2cc
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-12-12 18:18:04 +0530

    fixed the issue with view engine

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
-const exphbs = require('express-handlebars');
+const ejs = require('ejs');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
@@ -15,17 +15,24 @@ let users = require('./routes/users');
 // Creating an express app
 const app = express();
 
-// View Engine
-app.set('view', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-app.set('view engine', 'handlebars');
-
 // Defining node port.
 const port = process.env.port || 3000;
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// MySQL connection configuration
+const con = mysql.createConnection({
+    host : "127.0.0.1" || "localhost",
+    user : process.env.user || "root",
+    password : process.env.dbpassword || '',
+    database : "incridea"
+});
 
 // Setting up static folder
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -55,14 +62,6 @@ app.use((req, res, next) => {
 // Adding routes to the express app
 app.use('/', home);
 app.use('/users', users);
-
-// MySQL connection configuration
-const con = mysql.createConnection({
-    host : "127.0.0.1" || "localhost",
-    user : process.env.user || "root",
-    password : process.env.dbpassword || '',
-    database : "incridea"
-});
 
 // Error handing
 app.get('*', function(req, res, next) {
