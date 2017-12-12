9a1a9f7325c9e35fb817e19f90bef540a32d08ef
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-12-12 18:46:08 +0530

    Added Index page and fixed popper.min.js issue

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 let router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send('Hello from index route');
-});
+router.get('/', (req,res) => res.render('index'));
 
 module.exports = router;
