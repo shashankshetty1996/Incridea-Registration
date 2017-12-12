4fe071e0c6300ec7dbd05af02ed8a934a6f2e335
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-12-12 21:59:34 +0530

    Navigation Bar and about us page tempory page was set

--- public/js/controllers/aboutController.js
+++ public/js/controllers/aboutController.js
@@ -1,3 +1,7 @@
 angular.module('myApp').controller('aboutController',function($scope,$http) {
     $scope.msg='We are the incridea fest registration workshop';
+    $scope.developer = [
+        {"name":"Shashank S Shetty", "mob":"9481752839", "link":"https://github.com/shashankshetty1996/", "photo" : "img/img_avatar1.png"},
+        {"name":"Shashank S Shetty", "mob":"9481752839", "link":"https://github.com/shashankshetty1996/", "photo" : "img/img_avatar1.png"}
+    ];
 });
