dac1971c26e257cf4fe2b0ac56ec858c2aaa6546
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-12-13 22:21:14 +0530

    Done with login.ejs UI

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -2,11 +2,13 @@ angular.module('myApp').controller('loginController',function($scope,$http) {
     $scope.msg='Login Page';
 
     $scope.login = function() {
-        let params = {
+        $scope.params = {
             "username" : $scope.username,
             "password" : $scope.password
         };
+        console.log($scope.params);
 
+        // Setting fields blank again
         $scope.username = "";
         $scope.password = "";
     }
