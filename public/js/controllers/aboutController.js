ff2257d49b0c1aa5cb8cd67ac2ed7505ebb2738d
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-12-13 20:55:42 +0530

    Made changes in about us page added category

--- public/js/controllers/aboutController.js
+++ public/js/controllers/aboutController.js
@@ -1,7 +1,18 @@
 angular.module('myApp').controller('aboutController',function($scope,$http) {
-    $scope.msg='We are the incridea fest registration workshop';
-    $scope.developer = [
-        {"name":"Shashank S Shetty", "mob":"9481752839", "link":"https://github.com/shashankshetty1996/", "photo" : "img/img_avatar1.png"},
-        {"name":"Shashank S Shetty", "mob":"9481752839", "link":"https://github.com/shashankshetty1996/", "photo" : "img/img_avatar1.png"}
+    $scope.team = [
+        {
+            "name":"Shashank S Shetty", 
+            "mob":"9481752839", 
+            "link":"https://github.com/shashankshetty1996/", 
+            "photo" : "img/img_avatar1.png", 
+            "type" : "developer"
+        },
+        {
+            "name":"Shashank S Shetty", 
+            "mob":"9481752839", 
+            "link":"https://www.facebook.com/shashankshetty1996", 
+            "photo" : "img/img_avatar1.png", 
+            "type" : "core"
+        }
     ];
 });
