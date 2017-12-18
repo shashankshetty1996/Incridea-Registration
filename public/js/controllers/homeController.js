690c7795ee4c7bdc7d6678f4b3f31fd844e9f3f8
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-12-18 18:58:53 +0530

    Added credentials temper service with naviagation bar manpulations

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,3 +1,14 @@
-angular.module('myApp').controller('homeController',function($scope,$http) {
-    $scope.msg='Welcome to from incridea fest';
+angular.module('myApp').controller('homeController',function($scope, $http, credentialsService) {
+    $scope.msg='Welcome from INCRIDEA';
+
+    $scope.login = credentialsService.login;
+    $scope.admin = credentialsService.admin;
+
+    $scope.changeAdmin = () => {
+        $scope.admin = credentialsService.toggleAdmin();        
+    }  
+    
+    $scope.changeLogin = () => {
+        $scope.admin = credentialsService.toggleLogin();        
+    }  
 });
