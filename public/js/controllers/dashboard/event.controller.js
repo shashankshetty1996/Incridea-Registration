217cc7fc51b865e85ca1ef531b8aeb75049667ab
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-01-31 10:48:48 +0530

    added college list

--- public/js/controllers/dashboard/event.controller.js
+++ public/js/controllers/dashboard/event.controller.js
@@ -2,7 +2,11 @@ angular
     .module('myApp')
     .controller('dashboardEventController', dashboardEventController);
 
-dashboardEventController.$inject = ['$scope', '$location', 'ParticipantsService', 'CollegeService'];
-function dashboardEventController($scope, $location, ParticipantsService, CollegeService) {
-
+dashboardEventController.$inject = ['$scope', 'DashboardService'];
+function dashboardEventController($scope, DashboardService) {
+    DashboardService.GetCollegeList(function(response) {
+        if(response.success) {
+            $scope.collegeList = response.message;
+        }
+    });
 }
